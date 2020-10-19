4f37e78991198fcffba0c084d8b3d6b9ff3640cb
Thatgittyfeeling <kieran.mccahon@dairynz.co.nz>
2020-10-20 10:44:13 +1300

    Initial commit

--- Test_script.R
+++ Test_script.R
@@ -1,1 +1,8 @@
-#Test script demonstration 
+#Test script demonstration funi 
+#install.packages("usethis")
+
+library(usethis)
+use_git_config(user.name = "Thatgittyfeeling", user.email = "kieran.mccahon@dairynz.co.nz")
+
+
+usethis::use_git()
